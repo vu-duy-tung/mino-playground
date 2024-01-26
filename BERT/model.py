589885ad9cc5a59db3d3456e51f4d787d57ac810
vu-duy-tung <vuduycun202@gmail.com>
2024-01-26 21:26:25 +0700

    Update MultiheadAttention class

--- BERT/model.py
+++ BERT/model.py
@@ -40,3 +40,22 @@ class AttentionHead(nn.Module):
         
         return context
         
+        
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, dim_inp, dim_out):
+        super(MultiHeadAttention, self).__init__()
+        
+        self.heads = nn.ModuleList([
+            AttentionHead(dim_inp, dim_out) for _ in range(num_heads)
+        ])
+        
+        self.linear = nn.Linear(dim_out * num_heads, dim_inp)
+        self.norm = nn.LayerNorm(dim_inp)
+        
+    
+    def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor=None):
+        s = [head(input_tensor, attention_mask) for head in self.heads]
+        scores = torch.cat(s, dim=-1)
+        scores = self.linear(scores)
+        return self.norm(scores)
+    
